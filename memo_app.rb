1a7dabacd433bc8ec8160ffd32409aeae4f71cf3
Masashi Hasegawa <53898556+hiroshima820@users.noreply.github.com>
2021-10-27 23:41:31 +0900

    Topページを追加

--- memo_app.rb
+++ memo_app.rb
@@ -1,1 +1,40 @@
+# frozen_string_literal: true
 
+require 'json'
+require 'sinatra'
+require 'sinatra/reloader'
+
+FILE_PATH = 'db/memos.json'
+
+get '/' do
+  redirect '/memos'
+end
+
+get '/memos' do
+  @title = 'top'
+  erb :top
+end
+
+get '/new' do
+
+end
+
+post '/memos' do
+
+end
+
+get '/memos/:id' do |id|
+
+end
+
+delete '/memos/:id' do |id|
+
+end
+
+patch '/memos/:id' do |id|
+
+end
+
+get '/memos/:id/edit' do |id|
+
+end
